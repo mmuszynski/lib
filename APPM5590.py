4bb57f6f68195455aef963a09f2a48d569786747
Matt <mrmuszynski@gmail.com>
2018-02-21 13:33:05 -0700

    added some stuff to MLR. In particular, I changed the way MLR handles which data to keep and which to throw away. Now when calling it regress and partialRegress, you can specify which column has the Y data and which (if any) has the labels for the rows of data. The label part could be useful in creating some interactive plots.

--- APPM5590.py
+++ APPM5590.py
@@ -10,8 +10,283 @@
 #
 ###############################################################################
 
-from numpy import sqrt
+from numpy import sqrt, genfromtxt, hstack, ones, hstack, array, diag
+from numpy import setdiff1d, arange
+from numpy.linalg import inv
+import pdb
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+class MLR:
+	
+	def __init__(self):
+		self.X = -1
+		self.Y = -1
+		self.betaHat = -1
+		self.partialRegressionSwitches = []
+		self.partialRegressions = []
+
+	def parseData(self,coeffSwitch,rowLabelCol,yCol):
+		'''!
+		Note: rowLabelCol is a switch to tell the method whether or not
+		to use the first column as labels for the rows. Sometimes it's
+		used as part of an index.
+		'''
+		data = genfromtxt(self.dataFile)
+		self.rawData = data
+		tossTheseColumns = hstack(
+			[array([rowLabelCol,yCol]),coeffSwitch])
+		allColumns = arange(data.shape[1])
+		allRows = arange(data.shape[0])
+		keepTheseColumns = setdiff1d(allColumns,tossTheseColumns)
+		headerRow = 0
+		tossTheseRows = array([headerRow])
+		keepTheseRows = setdiff1d(allRows,tossTheseRows)
+
+		headers = genfromtxt(self.dataFile,dtype=str)[0,keepTheseColumns]
+		self.yName = genfromtxt(self.dataFile,dtype=str)[0,yCol]
+		self.dataNames = headers
+		self.Y = data[keepTheseRows,yCol,None]
+		self.X = hstack([ones(len(keepTheseRows)).reshape(-1,1),
+					data[keepTheseRows][:,keepTheseColumns]])
+		if rowLabelCol != 1:
+			self.rowNames = data[:,rowLabelCol,None]
+		else:
+			rowLabelCol = -1
+
+	def calculateBetaHat(self):
+		'''!
+		RABE Chapter 3 eqn A.3. Page 82
+		'''
+		Y = self.Y
+		X = self.X
+
+		self.betaHat = inv(X.T.dot(X)).dot(X.T).dot(Y)
+
+	def calculateYhat(self):
+		'''!
+		RABE Chapter 3 eqn A.4. Page 82
+		'''
+		X = self.X
+		betaHat = self.betaHat
+		self.Yhat = X.dot(betaHat)
+
+	def calculateHMatrix(self):
+		'''!
+		RABE Chapter 3 eqn A.5. Page 83
+		'''
+		X = self.X
+		self.H = X.dot(inv(X.T.dot(X))).dot(X.T)
+
+
+	def calculateResiduals(self):
+		'''!
+		RABE Chapter 3 eqn A.6. Page 83
+		'''
+		self.e = self.Y - self.Yhat
+
+	def calculateCMatrix(self):
+		'''!
+		RABE Chapter 3 eqn A.7. Page 83
+		'''
+		X = self.X
+		self.C = inv(X.T.dot(X))
+
+	def calculateSigmaHat(self):
+		e = self.e
+		n = len(self.Y)
+		p = len(self.betaHat) - 1
+		self.sigmaHat = sqrt((e.T.dot(e)/(n-p-1))[0,0])
+
+	def calculateSeBeta(self):
+		sigmaHat = self.sigmaHat
+		C = self.C
+		self.seBetaJ = sigmaHat*sqrt(diag(C).reshape(-1,1))
+
+	def calculateSSE(self):
+		'''!
+		RABE 3.26
+		'''
+		e = self.e
+		self.SSE = e.T.dot(e)[0,0]
+
+	def calculateSST(self):
+		'''!
+		RABE 2.44
+		'''
+		from numpy import mean
+		Y = self.Y
+		self.SST = (Y - mean(Y)).T.dot(Y - mean(Y))[0,0]
+
+	def calculateSSR(self):
+		'''!
+		RABE 2.44
+		'''
+		from numpy import mean
+		Yhat = self.Yhat
+		Y = self.Y
+		self.SSR = (Yhat - mean(Y)).T.dot(Yhat - mean(Y))[0,0]
+
+	def calculateRSq(self):
+		'''!
+		RABE 3.17
+		'''
+		self.rSq = self.SSR/self.SST
+
+	def calculateTTest(self):
+		self.tTest = self.betaHat/self.seBetaJ
+
+	def calculateStudentizedResiduals(self):
+		e = self.e
+		sigmaHat = self.sigmaHat
+		h = diag(self.H).reshape(-1,1)
+		r = e/(sigmaHat*sqrt(1-h))
+		self.r = r
+
+
+	def regress(self,**kwargs):
+		try:
+			coeffSwitch = kwargs['coeffSwitch']
+		except:
+			coeffSwitch = array([-1])
+		try:
+			rowLabelCol = kwargs['rowLabelCol']
+		except:
+			rowLabelCol=-1
+		try:
+			yCol = kwargs['yCol']
+		except:
+			yCol = 0
+
+		self.parseData(coeffSwitch,rowLabelCol,yCol)
+		self.calculateBetaHat()
+		self.calculateYhat()
+		self.calculateHMatrix()
+		self.calculateResiduals()
+		self.calculateCMatrix()
+		self.calculateSigmaHat()
+		self.calculateSeBeta()
+		self.calculateSST()
+		self.calculateSSR()
+		self.calculateSSE()
+		self.calculateRSq()
+		self.calculateTTest()
+		self.calculateStudentizedResiduals()
+
+	def partialRegress(self,coeffSwitch,**kwargs):
+		try:
+			rowLabelCol = kwargs['rowLabelCol']
+		except:
+			rowLabelCol = -1
+
+		try:
+			yCol = kwargs['yCol']
+		except:
+			yCol = 0
+
+		partialRegression = MLR()
+		partialRegression.dataFile = self.dataFile
+		partialRegression.regress(
+			coeffSwitch=coeffSwitch,
+			rowLabelCol=rowLabelCol,
+			yCol=yCol
+			)
+		self.partialRegressionSwitches.append(coeffSwitch)
+		self.partialRegressions.append(partialRegression)
+
+	def scatterPlot(self,regression):
+		if regression.X.shape[1] != 2:
+			print('Error. Scatter only available for 2D data.')
+			return
+		plt.plot(regression.X[:,1],regression.Y,'.',label='Raw Data')
+		plt.plot(regression.X[:,1],regression.Yhat,'r',label='Fitted Data')
+		plt.ylabel(regression.yName)
+		plt.xlabel(regression.dataNames[0])
+		plt.title(regression.yName + \
+			' versus ' + regression.dataNames[0])
+
+
+
+	def scatterAllPartials(self):
+		for i in range(0,len(self.partialRegressions)):
+			plt.figure()
+			self.scatterPlot(self.partialRegressions[i])
+
+	def scatter3D(self):
+		if self.X.shape[1] != 3:
+			print('Error. 3D scatter only available for 3D data.')
+			return
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		ax.scatter(
+			self.X[:,1], 
+			self.X[:,2], 
+			self.Y.T[0], label='Raw Data')
+		ax.scatter(
+			self.X[:,1], 
+			self.X[:,2], 
+			self.Yhat.T[0], 
+			color='r',label='Fitted Data')
+		ax.set_xlabel(self.dataNames[0])
+		ax.set_ylabel(self.dataNames[1])
+		ax.set_zlabel(self.yName)
+
+	def plotMatrix(self):
+		fullData = hstack([self.Y,self.X[:,1:]])
+		dim = fullData.shape[1]
+		fig, axes = plt.subplots(
+			dim, dim, sharex='col', sharey='row')
+		names = hstack([self.yName,self.dataNames])
+		for i in range(0,dim):
+			for j in range(0,dim):
+				if i == j:
+					text = names[i]
+					axes[i,j].text(0.5, 0.5,text,
+						horizontalalignment='center',
+						verticalalignment='center',
+						transform=axes[i,j].transAxes)
+				elif i > j:
+					text = "{:.3f}".format(
+						cor(fullData[:,i],fullData[:,j])
+						)
+					
+					axes[i,j].text(0.5, 0.5,text,
+						horizontalalignment='center',
+						verticalalignment='center',
+						transform=axes[i,j].transAxes)
+
+				else:
+					axes[i,j].plot(fullData[:,j],fullData[:,i],'.')
+
+
+	def plotResidualsVPredictors(self,dims):
+		X = self.X[:,1:]
+		r = self.r
+		fig, axes = plt.subplots(
+			dims[0], dims[1], sharex=False, sharey=True)
+		plt.suptitle('Standardized (Studentized) Residuals versus Predictor Variables')
+		plt.subplots_adjust(hspace=0.5)
+		for i in range(0,dims[0]):
+			for j in range(0,dims[1]):
+				axes[i,j].plot(X[:,i*dims[1]+j],r,'.')
+				axes[i,j].set_xlabel(self.dataNames[i*dims[1]+j])
+				axes[i,j].set_ylabel('Standardized Residuals')
+	def plotResidualsVFitted(self):
+		Yhat = self.Yhat
+		r = self.r
+		plt.figure()
+		plt.plot(Yhat,r,'.')
+		plt.title('Standardized (Studentized) Residuals versus Fitted Values')
+		plt.xlabel('Fitted Values')
+		plt.ylabel('Standardized Residuals')
+
+	def plotStandardizedResiduals(self):
+		r = self.r
+		plt.figure()
+		plt.plot(r,'.')
+		plt.title('Standardized (Studentized) Residuals versus Observation Index')
+		plt.xlabel('Index')
+		plt.ylabel('Standardized Residual')
 def bar(X):
 	'''!
 	It's a mean, duh. Just doing this for pedagogical reasons.
